7538f116029314b385b32fb1aa853e658e685fe4
httpsgaab <gabygomes.2303.2007@gmail.com>
2024-01-28 16:57:49 -0300

    Initial commit

---
+++ teste.py
@@ -0,0 +1,8 @@
+login=input("Digite seu login:")
+senha=input("Senha:")
+
+if login=="gaab" and senha==40028922:
+    print("isso")
+
+else:
+    ("se fudeu")
---
+++ verificacao.php
@@ -0,0 +1,13 @@
+<?php
+$usuario= $_POST["usuario"];
+$senha= $_POST["senha"];
+
+if ($usurio="gilda" && $senha=1234) {
+    print ("Acesso Liberado!");
+}
+elseif ($usurio="juan" && $senha=1234) {
+    print ("Acesso Liberado!");
+}
+elseif ($usurio="gabriella" && $senha=1234) {
+    print ("Acesso Liberado!");
+}
